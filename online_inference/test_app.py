f7a9ec12f7fca1f8fe95b595e737cc7490310467
Ilya Zuev <ilya.zuev@rt.ru>
2022-11-24 19:51:09 +0300

    Add test for predict query

---
+++ online_inference/test_app.py
@@ -0,0 +1,32 @@
+import csv
+from fastapi.testclient import TestClient
+
+from .app import app, Data
+
+
+PATH_TO_DATA = "./data/test_data/test.csv"
+PATH_TO_ANSWER = "./data/test_data/answer.csv"
+
+client = TestClient(app)
+
+
+def test_predict():
+    with open(PATH_TO_DATA) as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=",")
+        data = [row for row in csvreader]
+
+    with open(PATH_TO_ANSWER) as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=",")
+        answer = [int(row[0]) for row in csvreader]
+
+    test_data = {
+        "data": data
+    }
+
+    response = client.post(
+        "/predict",
+        json=test_data,
+    )
+
+    assert response.status_code == 200
+    assert response.json() == answer
